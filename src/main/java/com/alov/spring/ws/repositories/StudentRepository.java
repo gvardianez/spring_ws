432f24436f5b5d3708cebc76f3a57ee59b850081
Aleksandr <gvardianez@yandex.ru>
2022-04-12 14:28:35 +0300

    init

---
+++ src/main/java/com/alov/spring/ws/repositories/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.alov.spring.ws.repositories;
+
+import com.alov.spring.ws.entities.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+    @Query("select s from StudentEntity s where s.name = ?1")
+    Optional<StudentEntity> findByName(String name);
+}
